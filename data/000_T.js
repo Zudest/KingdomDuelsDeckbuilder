0fcc7eba9f737e000c63bea98b528e12459aa31a
Zudest <zudest.info@gmail.com>
2025-01-04 20:52:46 -0300

    (Add) Terrains now supported at least for checking them online

---
+++ data/000_T.js
@@ -0,0 +1,28 @@
+const EXTRA_TERRAINS = {
+    "cards": [
+        {"name": "Land","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-01","image": "./cards/_/01.jpg","deck": "C","duelist": ""}
+        ,{"name": "Arena","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-02","image": "./cards/_/02.jpg","deck": "C","duelist": ""}
+        ,{"name": "Forest","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-03","image": "./cards/_/03.jpg","deck": "C","duelist": ""}
+        ,{"name": "Wasteland","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-04","image": "./cards/_/04.jpg","deck": "C","duelist": ""}
+        ,{"name": "Mountain","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-05","image": "./cards/_/05.jpg","deck": "C","duelist": ""}
+        ,{"name": "Meadow","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-06","image": "./cards/_/06.jpg","deck": "C","duelist": ""}
+        ,{"name": "Sea","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-07","image": "./cards/_/07.jpg","deck": "C","duelist": ""}
+        ,{"name": "Rugged Coast","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-08","image": "./cards/_/08.jpg","deck": "C","duelist": ""}
+        ,{"name": "Night","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-09","image": "./cards/_/09.jpg","deck": "C","duelist": ""}
+        ,{"name": "Lake","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-10","image": "./cards/_/10.jpg","deck": "C","duelist": ""}
+        ,{"name": "Virtual Simulation Field","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-11","image": "./cards/_/11.jpg","deck": "C","duelist": ""}
+        ,{"name": "Graveyard","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-12","image": "./cards/_/12.jpg","deck": "C","duelist": ""}
+        ,{"name": "Labyrinth","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-13","image": "./cards/_/13.jpg","deck": "C","duelist": ""}
+        ,{"name": "Castle Grounds","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-14","image": "./cards/_/14.jpg","deck": "C","duelist": ""}
+        ,{"name": "Desert","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-15","image": "./cards/_/15.jpg","deck": "C","duelist": ""}
+        ,{"name": "Cave","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-16","image": "./cards/_/16.jpg","deck": "C","duelist": ""}
+        ,{"name": "Vortex","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-17","image": "./cards/_/17.jpg","deck": "C","duelist": ""}
+        ,{"name": "Volcano","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-18","image": "./cards/_/18.jpg","deck": "C","duelist": ""}
+        ,{"name": "Ruins","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-19","image": "./cards/_/19.jpg","deck": "C","duelist": ""}
+        ,{"name": "Dragon Valley","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-20","image": "./cards/_/20.jpg","deck": "C","duelist": ""}
+        ,{"name": "Dungeon","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-21","image": "./cards/_/21.jpg","deck": "C","duelist": ""}
+        ,{"name": "Swamp","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-22","image": "./cards/_/22.jpg","deck": "C","duelist": ""}
+        ,{"name": "Icefield","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-23","image": "./cards/_/23.jpg","deck": "C","duelist": ""}
+        ,{"name": "Palace","frame": "Terrain","types": "Terrain","set": "Terrains","cardid": "_-24","image": "./cards/_/24.jpg","deck": "C","duelist": ""}
+    ]
+};
